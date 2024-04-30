5dc97f1980de5e43085143029557f29788987c1a
samsnwn <samuel.c.glantz@gmail.com>
2024-04-30 16:16:29 +0200

    Makefile

--- Makefile
+++ Makefile
@@ -1,6 +1,27 @@
+SRCS = ft_printf.c
+
+HEAD = printf.h
+OBJS = ${SRCS:.c=.o}
 NAME = libftprintf.a
 CC = cc
 RM = rm -f
 CFLAGS = -Wall -Werror -Wextra
 
 all : ${NAME}
+
+${NAME} : ${OBJS}
+	ar rc ${NAME} ${OBJS}
+	ranlib ${NAME}
+
+%.o: %.c
+	${CC} ${CFLAGS} -c $< -o $@
+
+clean : 
+	${RM} ${OBJS}
+
+fclean : clean
+	${RM} ${NAME}
+
+re : fclean all
+
+.PHONY : all clean fclean re
--- printf.h
+++ printf.h
@@ -1,34 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   printf.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: samcasti <samcasti@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/30 16:15:50 by samcasti          #+#    #+#             */
-/*   Updated: 2024/04/30 16:15:52 by samcasti         ###   ########.fr       */
+/*   Created: 2024/04/30 16:15:57 by samcasti          #+#    #+#             */
+/*   Updated: 2024/04/30 16:15:59 by samcasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	count_args(const char *str);
+#ifndef PRINTF_H
+# define PRINTF_H
 
-int	ft_printf(const char *, ...)
-{
-	// loop over char* until we find a%,then we stop and say: this will be one argument.
-	// count_args(const char *);
-	return (0);
-}
-
-int	count_args(const char *str)
-{
-	int i = 0;
-	int count = 0;
-
-	while (str[i])
-	{
-		if (str[i] == '%')
-			count++;
-		i++;
-	}
-	return (count);
-}
+#endif
